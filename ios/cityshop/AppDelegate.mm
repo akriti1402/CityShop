b8157ab622ad8cff8e4e846e311b37697ee4de86
Akriti Rai <147941324+akriti1402@users.noreply.github.com>
2025-08-03 20:06:00 +0530

    Update AppDelegate.mm

--- ios/cityshop/AppDelegate.mm
+++ ios/cityshop/AppDelegate.mm
@@ -7,8 +7,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"cityshop";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
+ 
   self.initialProps = @{};
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
@@ -29,3 +28,4 @@
 }
 
 @end
+
